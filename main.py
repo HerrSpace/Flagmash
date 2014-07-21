13dd07500a15dbfe73a673ca76b0011a2e5b6684
Patrick Meyer <space@potential-terrorist.com>
2014-07-21 23:40:11 +0200

    fix standalone

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from flag_app.PageHandler import Root
 if __name__ == '__main__':
 	ip = '127.0.0.1'
 	port = 8081
-	static_root = os.path.abspath(os.path.join(os.path.dirname(__file__), 'static'))
+	static_root = os.path.abspath(os.path.dirname(__file__))
 
 	cherrypy.config.update({'server.socket_host': ip})
 	cherrypy.config.update({'server.socket_port': port})
@@ -18,15 +18,17 @@ if __name__ == '__main__':
 	d = cherrypy.dispatch.RoutesDispatcher()
 	d.connect(name='root',		action='index',		controller=example_controller,	route='/')
 	d.connect(name='root',		action='index_r',	controller=example_controller,	route='/:game/:winner')
-	d.connect(name='root',		action='stats',	controller=example_controller,	route='/stats')	
+	d.connect(name='root',		action='stats',		controller=example_controller,	route='/stats')	
 
 	config_dict = {
 		'/': {
 			'request.dispatch': d,
-			'tools.staticdir.on': False,
-			'tools.staticdir.root': static_root,
-			'tools.staticdir.dir': '.',
 			'request.show_tracebacks': False
+		},
+		'/static': {
+			'tools.staticdir.on': True,
+			'tools.staticdir.root': static_root,
+			'tools.staticdir.dir': 'static'
 		}
 	}
 
